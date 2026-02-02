3d05c7f3d933343236f4cab2bca3c896cfb52577
Alona Kuznietsova <acvetochka@gmail.com>
2026-02-02 12:47:39 +0100

    add ConsoleApp and WinFormApp from Day1

---
+++ Day1/ConsoleApp/ConsoleApp/Program.cs
@@ -0,0 +1,2 @@
+﻿Console.OutputEncoding = System.Text.Encoding.UTF8;
+Console.WriteLine("Привіт, мене звати Альона");
---
+++ Day1/WinFormsApp/WinFormsApp/Form1.cs
@@ -0,0 +1,15 @@
+namespace WinFormsApp
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Привіт, мене звати Альона");
+        }
+    }
+}
