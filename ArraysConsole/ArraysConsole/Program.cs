2659b3888399c67e680b7476628141a4f6431288
Alona Kuznietsova <acvetochka@gmail.com>
2026-02-04 15:39:50 +0100

    add ArraysConsole

---
+++ ArraysConsole/ArraysConsole/Program.cs
@@ -0,0 +1,17 @@
+﻿//Завдання 1
+//Створіть консольний застосунок.
+//Дано масив int[] values = { 10, 123, 56, 39, 67, 188 }
+//;.
+//Напишіть код, що порахує суму усіх значень масиву values.
+//Виведіть суму на екран.
+
+int[] values = { 10, 123, 56, 39, 67, 188 };
+int total = 0;
+
+for(int i = 0;  i < values.Length; i++)
+{
+    //Console.WriteLine(Convert.ToString(total) + "+" + Convert.ToString(values[i]) + "=");
+    total += values[i];
+    
+}
+    Console.WriteLine(total);
